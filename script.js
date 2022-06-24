770c5edb0475502d368865a32655604ad58fec88
unaPeace <84766354+unaPeace@users.noreply.github.com>
2022-06-24 12:40:16 +0200

    yes

--- script.js
+++ script.js
@@ -49,3 +49,15 @@ $(document).ready(function() {
   });
 
 });
+
+var $grid = $('.grid').masonry({
+  // set itemSelector so .grid-sizer is not used in layout
+  itemSelector: '.grid-item',
+  // use element for option
+  columnWidth: '.grid-sizer',
+  percentPosition: true	
+})
+
+$grid.imagesLoaded().progress( function() {
+  $grid.masonry();
+});
